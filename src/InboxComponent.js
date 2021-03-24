4d712405e0df34d119bf9434503259e6a8d6a26a
Akshat Dhankher <akshat.dhankher@gmail.com>
2021-03-24 11:14:46 -0400

    More chat progress (kinda)

--- src/InboxComponent.js
+++ src/InboxComponent.js
@@ -3,6 +3,7 @@ import Talk from "talkjs";
 
 import "./styles.css";
 import {useState} from 'react';
+//import SampleData from "./SampleData"
 
 // Creates a TalkJS user assuming that the currentUser is passed as a prop to
 // our component. The Talk.User object is used to synchronize user data with
@@ -34,22 +35,36 @@ class InboxComponent extends Component {
           appId: "tNfx4wm8",  //This is a custom appId generated from our TalkJS trial
           me: me
         });
+        // window.talkSession.setDesktopNotificationEnabled("");
       }
 
-      // Hard-coded dummy user to test chatting with
+      // Hard-coded dummy users to test chatting with
       var johnSmith = new Talk.User({
         id: "54321",
         name: "John Smith",
         email: "johnsmith@example.com",
         photoUrl: "https://demo.talkjs.com/img/sebastian.jpg",
-        welcomeMessage: "Hey, how can I help?"
+        welcomeMessage: "Hi! How's it going?"
+      });
+
+      var lucyEaton = new Talk.User({
+        id: "01010",
+        name: "Lucy Eaton",
+        email: "lucyeaton@example.com",
+        photoUrl: "https://thumbs.dreamstime.com/b/friendly-adult-casual-dressed-business-woman-student-girl-standing-straight-headshot-portrait-office-183889436.jpg",
+        welcomeMessage: "Hello! Good to see you in class today :)"
       });
 
       // Creating sample conversation. oneOnOneId is a helper method that
       // generates a unique conversation ID for a given pair of users.
-      var conversation = window.talkSession.getOrCreateConversation(Talk.oneOnOneId(me, johnSmith))
+      var conversation = window.talkSession.getOrCreateConversation(Talk.oneOnOneId(me, johnSmith));
       conversation.setParticipant(me);
       conversation.setParticipant(johnSmith);
+
+      // Creating some more filler coversations with our SampleData
+      var convoLucy = window.talkSession.getOrCreateConversation(Talk.oneOnOneId(me, lucyEaton))
+      convoLucy.setParticipant(me);
+      convoLucy.setParticipant(lucyEaton);
 
       // Creating an inbox
       var inbox = window.talkSession.createInbox({selected: conversation});
@@ -58,7 +73,10 @@ class InboxComponent extends Component {
   }
 
   render() {
-    return (<div style={{height: '500px'}} ref={this.talkjsContainer} className="InboxComponent"></div>)
+    return (
+      <div style={{height: '500px'}} ref={this.talkjsContainer} className="InboxComponent">
+      </div>
+    )
   }
 
 }
---
+++ src/SampleData.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Talk from "talkjs";
+import "./styles.css";
+
+// Dummy users to populate the chats
+var lucyEaton = new Talk.User({
+  id: "01010",
+  name: "Lucy Eaton",
+  email: "lucyeaton@example.com",
+  photoUrl: "https://thumbs.dreamstime.com/b/friendly-adult-casual-dressed-business-woman-student-girl-standing-straight-headshot-portrait-office-183889436.jpg",
+  welcomeMessage: "Hello! Good to see you in class today :)"
+});
+
+var edJones = new Talk.User({
+  id: "12321",
+  name: "Ed Jones",
+  email: "edjones@example.com",
+  photoUrl: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-4.png",
+  welcomeMessage: "Hey have you started the assignment yet?"
+})
+
+
+export {lucyEaton};
