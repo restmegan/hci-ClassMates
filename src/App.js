6d99390738051649f2a9e0301d8b9d2e872bc803
restmegan <57573768+restmegan@users.noreply.github.com>
2021-03-31 18:44:26 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import "./styles.css";
-import {useState} from 'react';
-import "milligram";
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useRouteMatch,
+  useParams
 } from "react-router-dom";
-
+import "milligram";
+import CalendarPage from "./CalendarPage";
+import ChatPage from "./ChatPage";
 
 export default function App() {
-
-  const [buttonText, setButtonText] = useState("this is a button");
-
-  function handleClick(){
-   setButtonText("you clicked!");
-  }
-
-
   return (
-    <div className="App">
-      <h1>ClassMates</h1>
+    <Router>
+      <div>
+        <h1>ClassMates</h1>
+        <h6><Link to="/">Home</Link></h6>
+        <Switch>
+          <Route path="/chat">
+            <ChatPage />
+          </Route>
+          <Route path="/calendar">
+            <CalendarPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <div class="row">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/chat">Chat</Link>
+          </li>
+          <li>
+            <Link to="/calendar">Calendar</Link>
+          </li>
+        </ul>
+        </div>
+
+        
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+  <div className="App">
       <div className="row">
         <div className="column box">
           <h3>Happening Now</h3>
@@ -28,16 +58,12 @@ export default function App() {
           <p> more space</p>
         </div>
         <div className="column">
-          <h3>Chats</h3>
+          <h3><Link to="/chat">Chat</Link></h3>
         </div>
         <div className="column">
-          <h3>Schedule</h3>
+          <h3><Link to="/calendar">Calendar</Link></h3>
         </div>
       </div>
     </div>
   );
-}
-
-// Provides the basic home-page render
-function Home() {
 }
