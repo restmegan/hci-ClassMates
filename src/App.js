cd865246ed3bd8916846e4527193499588356d94
restmegan <57573768+restmegan@users.noreply.github.com>
2021-03-31 17:16:23 -0400

    homepage formatting start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import {useState} from 'react';
+import "milligram";
 
 export default function App() {
 
@@ -16,6 +17,20 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
       <p>hi</p>
     <button onClick={handleClick}>{buttonText}</button>
+      <h1>ClassMates</h1>
+      <div className="row">
+        <div className="column box">
+          <h3>Happening Now</h3>
+          <p> blank</p>
+          <p> more space</p>
+        </div>
+        <div className="column">
+          <h3>Chats</h3>
+        </div>
+        <div className="column">
+          <h3>Schedule</h3>
+        </div>
+      </div>
     </div>
   );
 }
