116ab7d4c849ea3539a91deb1e1dba5e64745aae
Akshat Dhankher <akshat.dhankher@gmail.com>
2021-04-18 02:44:09 -0400

    Created/styled Home page

---
+++ src/ButtonJoinNow.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+// import './ButtonSubmit.css';
+
+const ButtonSubmit = styled.button`
+right: 20px;
+height: 30px;
+border: none;
+outline: none;
+background: #005fff;
+border-radius: 50px;
+font-family: Helvetica Neue, sans-serif;
+font-weight: bold;
+font-size: 15px;
+line-height: 18px;
+color: #ffffff;
+cursor: pointer;
+flex: 1;
+padding: 10px;
+padding-top: 5px;
+margin: 5px;
+margin-bottom: 15px;
+`;
+
+// max-width: 111px;
+
+// display: inline-block;
+// background-color: rgb(72, 28, 125);
+// color: #fafafa;
+// border-color: papayawhip;
+// font-size: 1em;
+// borser-style: none;
+// border-radius: 15%;
+// padding: 1rem;
+
+export default ({ name, onClick }) => {
+  return <ButtonSubmit id="buttonSubmit"
+  onClick={onClick}>{name}</ButtonSubmit>;
+};
